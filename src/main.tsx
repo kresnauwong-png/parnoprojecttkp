1601859d7155a16fb4ce09900ca8321fb7526287
kresnauwong-png <kresnauwong@gmail.com>
2026-07-19 13:47:03 +0700

    Import main CSS file in main.tsx
    Import main CSS file for styling.

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
+import './index.css' // Pastikan file CSS utama di-import di sini
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
